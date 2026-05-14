d08846349ee89c7c5c871ef8699f8b01a564ecb2
Felipe Mota <felipemota465@gmail.com>
2026-05-14 12:26:28 -0300

    Facil
    Facil

---
+++ Facil/covid19.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p, q;
+    cin >> p >> q;
+
+    if (p <= 50 && q <= 10) {
+        cout << "White\n";
+    }
+    else if (q > 30) {
+        cout << "Red\n";
+    }
+    else {
+        cout << "Yellow\n";
+    }
+
+    return 0;
+}
